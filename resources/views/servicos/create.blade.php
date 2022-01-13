014eea4d2ca7ad12a132ba3e468cd9e756362663
Einstein Xavier <einstein.xavier@gmail.com>
2022-01-13 12:21:45 -0300

    Adicionado mascaras no projeto

--- resources/views/servicos/create.blade.php
+++ resources/views/servicos/create.blade.php
@@ -7,7 +7,12 @@
 @stop
 
 @section('content')
+    @include('_mensagens')
     <form action="{{route('servicos.store')}}" method="post">
         @include('servicos._form')
     </form>
 @stop
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endsection
